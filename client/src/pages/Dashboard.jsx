ae9abc213b3c5e36f05a9682a32490af64b5a27e
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-01-14 11:30:37 +0530

    after the 10 hours fix bug of flowbite

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,18 @@ export default function Dashboard() {
   return (
     <div>
       Dashboard
-      
+      <Button color="blue">Blue</Button>
+      <>
+   
+   <h1>MY Vite flowbite project with React</h1>
+   <Button>Click me</Button>
+   <Button>Default</Button>
+      <Button color="blue">Blue</Button>
+      <Button color="gray">Gray</Button>
+      <Button color="dark">Dark</Button>
+      <Button color="light">Light</Button>
+      <Button color="success">Success</Button>
+   </>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
+
 /** @type {import('tailwindcss').Config} */
-import flowbite from "flowbite-react/tailwind";
-
-
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    ...flowbite.content(), // Use the spread operator for Flowbite content paths
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(), // Add Flowbite plugin
+    flowbite.plugin(),
+   
   ],
 };
