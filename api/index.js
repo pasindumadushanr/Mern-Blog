ffd77c87ba8484f5a2157bc7206fd71679a744c5
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-01-15 17:45:37 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,15 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);   
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) =>  {
+    const statuscode =err.statuscode || 500;
+    const message = err.message || 'Something went wrong';
+    res.status(statuscode).json({
+        
+        success: false,
+        statuscode,
+         message,
+        });
+          
+    }); 
