c1728a370e2baf8acf227845a0fdbbada4f0dd14
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-01-26 22:43:26 +0530

    add update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,7 +5,7 @@ import User from '../models/user.model.js';
 export const test = (req, res) => {
   res.json({ message: 'API is working!' });
 };
-
+//complicated function to update user
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
