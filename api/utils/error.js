490ac5f670d5e8d92cef1cca58ab3cc4f0c80705
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-01-26 22:38:08 +0530

    Fix the error of not show error msg

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
-export const errorHandler = (err, req, res, next) => (statusCode, message) => {
+export const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
     error.message = message;
-    next(error);
-};
+    return error;
+  };
